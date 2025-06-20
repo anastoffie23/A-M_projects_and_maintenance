e2971f253cfa3b74ec6eb2086d1e40999cdf44f9
anastoffie23 <131896160+anastoffie23@users.noreply.github.com>
2025-06-20 04:02:17 +0200

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.querySelector("form");
-  form.addEventListener("submit", function (e) {
-    alert("Thank you! Your message has been sent.");
-  });
-});
